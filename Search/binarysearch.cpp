243ab0bd62badfab24ed3d87032d94c51dde7b2c
Yashaswi D <33202992+yashaswid@users.noreply.github.com>
2018-10-27 11:39:59 -0700

    Add files via upload

---
+++ Search/binarysearch.cpp
@@ -0,0 +1,40 @@
+/*============================================================================= 
+#  Author:          yashaswi 
+#  FileName:        binarysearch.cpp 
+#  Description:     Binary search
+      
+=============================================================================*/ 
+
+#include<iostream>
+using namespace std;
+
+int binarysearch(int a[],int k, int first,int last)
+{
+
+	int mid=(first+last)/2;
+	if(a[mid]==k)
+	{
+		return mid;
+	}
+	if(k>a[mid])
+      {
+     return 	binarysearch(a,k,mid+1,last);
+	  }
+
+	else
+	{
+	return	binarysearch(a,k,first,mid-1);
+	}
+	return -1;
+}
+
+
+
+int main()
+{
+	int a[]={1,2,3,4,5,6};
+	int k=3;
+	int n=sizeof(a)/sizeof(a[0]);
+	int result=binarysearch(a,k,0,n-1);
+	cout<<"found at position"<<result;
+}
---
+++ Search/findinarray.cpp
@@ -0,0 +1,30 @@
+/*============================================================================= 
+#  Author:          yashaswi 
+#  FileName:        linearsearch.cpp 
+#  Description:     Linear search
+      
+=============================================================================*/ 
+#include<iostream>
+using namespace std;
+
+
+int find(int a[], int size,int k)
+{
+	
+	while(i<size && a[i]!=k)
+	{
+		i++;
+	}
+	return i;
+	
+}
+
+
+int main()
+{
+	int a[5]={2,3,4,6,7};
+	int size=5;
+	int k=3;
+int c=	find(a,size,k);
+cout<<c;
+	}
